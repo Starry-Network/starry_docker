6a9184fafd5c52a993913224391372a85f73450c
fuling <mon.samuel@qq.com>
2021-06-09 00:53:31 +0800

    update prepare script and docker-compose file

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,8 @@ git clone https://github.com/Starry-Network/starry_node.git node
 git clone https://github.com/Starry-Network/starry_query.git query
 git clone https://github.com/Starry-Network/app.git app
 
+mkdir .data
+
 cd query
 yarn install
 yarn codegen && yarn build
